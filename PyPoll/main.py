c361f28ad80a8a8764ce25b601dce339f6a1d0ff
gna0503 <67257143+gna0503@users.noreply.github.com>
2020-09-08 15:05:18 +1000

    print out data finished

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,7 +4,8 @@ import csv
 csv_path = os.path.join("Resources","02-Homework_03-Python_Instructions_PyPoll_Resources_election_data.csv")
 
 candidates = []
-candidates_total =[]
+candidates_total =[0,0,0,0]
+vote_percentage = [0,0,0,0]
 num_rows = 0
 
 
@@ -21,17 +22,17 @@ with open(csv_path) as csvfile:
         else:
             candidates_index = candidates.index(row[2])
             candidates_total[candidates_index] +=1
+
+total_votes = sum(candidates_total)
+
+for i in range(len(candidates)):
+    vote_percentage[i] = round(int(candidates_total[i])/int(total_votes)*100,3)
+    
+Winner = candidates[vote_percentage.index(max(vote_percentage))]
             
 
 print(candidates)
 print(candidates_total)
-
-
-        
-
-
-            
-
-total_votes = num_rows
-        
+print(vote_percentage)
+print(Winner)
 
